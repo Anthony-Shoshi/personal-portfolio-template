b1e65cd0179eddf2912b56bf176c62c40d19c023
Anthony Shoshi Gomes <anthonyshoshigomes1996@gmail.com>
2023-11-29 15:08:27 +0600

    contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import About from './components/About'
+import Contact from './components/Contact'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Portfolio from './components/Portfolio'
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Portfolio />
       <Testimonial />
+      <Contact />
     </>
   )
 }
